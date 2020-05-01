b3594baf4fddb0c45f47acf7499022aca2132dbc
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-05-01 22:59:02 +0100

    adds find(), findOne, and deleteOne function

--- server/helpers/createRouter.js
+++ server/helpers/createRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
 
 const createRouter = (collection) => {
   const router = express.Router();
@@ -6,6 +7,28 @@ const createRouter = (collection) => {
   router.get('/', (req, res) => {
     collection.find().toArray()
     .then(docs => res.json(docs))
+    .catch((error) => {
+      console.log(error);
+      res.status(500);
+      res.json({error: error})
+    })
+  });
+
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection.findOne({_id: ObjectID(id)})
+    .then(doc => res.json(doc))
+    .catch((error) => {
+      console.log(error);
+      res.status(500);
+      res.json({error: error})
+    })
+  });
+
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection.deleteOne({_id: ObjectID(id)})
+    .then(result => res.json(result))
   });
 
   return router;
--- server/server.js
+++ server/server.js
@@ -17,6 +17,6 @@ MongoClient.connect('mongodb://localhost:27017')
 })
 .catch(console.error);
 
-app.listen(3000, () => {
+app.listen(3000, function ()  {
   console.log(`Listening on port ${ this.address().port }`);
 });
